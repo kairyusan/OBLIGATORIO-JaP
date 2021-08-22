f28393dc01dfbfe2d70cad59aeba52b31fe01fc0
Kairyusan <nanoha462@gmail.com>
2021-08-22 20:56:46 -0300

    Ediciones para la Entrega 1

--- js/login.js
+++ js/login.js
@@ -4,3 +4,32 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
 });
+
+function ingreso(user, pass){  
+
+    if (user.trim()==="" || pass.trim()===""){ //Chequea que el dato recibido no esté vacío. 
+
+        alert("Ingrese datos correctos");
+    
+    }else{
+
+    localStorage.setItem("usuario", user.trim()); //setItem almacena el dato en la posición "usuario"
+    localStorage.setItem("password", pass.trim()); // Almaceno la contraseña
+    sessionStorage.setItem("usuario", user.trim());
+    location.href="inicio.html";
+    alert (" Bienvenido Usuario : " + user); 
+    
+    }
+}
+
+function laproximasale(){
+    alert("Anota el pass en papel para la próxima");
+    
+
+}
+
+function desconectate(){
+localStorage.clear();
+sessionStorage.clear();
+location.href="index.html";
+}
--- js/products.js
+++ js/products.js
@@ -1,6 +1,37 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+var currentProductsArray = [];
 
+function showProductsList() {
+
+    let htmlContentToAppend = "";
+    for (let i = 0; i < currentProductsArray.length; i++) {
+        let Prod = currentProductsArray[i];
+
+            htmlContentToAppend += ` 
+   <div class="card-group";>
+        <div class="card" id="prod-list-container">
+          <img class="card-img-top" src="${Prod.imgSrc}" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">${Prod.name}</h5>
+            <p class="card-text">${Prod.description}</p>
+            <h3 class="card-number">${Prod.cost} USD </h5>
+            <p class="card-text"><small class="text-muted">${Prod.soldCount} vendidos </small></p>
+            </div>
+        </div>
+    </div>
+`
+
+            document.getElementById("prod-list-container").innerHTML = (htmlContentToAppend);
+        }
+    }
+
+
+document.addEventListener("DOMContentLoaded", function (e) {
+    getJSONData(PRODUCTS_URL).then(function (resultObj) {
+        if (resultObj.status === "ok") {
+            currentProductsArray = resultObj.data;
+            showProductsList(resultObj.data)
+        }
+    });
 });
+
+
