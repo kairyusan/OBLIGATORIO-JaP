93d07dc7e799ae6af626f513a89abe7da393e98f
Kairyusan <nanoha462@gmail.com>
2021-08-30 03:42:51 -0300

    Correcciones login+oAuth, correcciones en album de productos + OBLIGATORIO2 COMPLETO

--- js/login.js
+++ js/login.js
@@ -1,35 +1,73 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
 
+
+function ingreso(user, pass) {
+  let usu = {};
+
+  if (user.trim() === "" || pass.trim() === "") {
+
+    alert("Ingrese datos correctos");
+
+  } else {
+    usu.nombre = user;
+    usu.estado = "conectado";
+    usu.contraseña = pass;
+    localStorage.setItem('user', JSON.stringify(usu));
+    sessionStorage.setItem('user', JSON.stringify(usu));
+    location.href = "inicio.html";
+    alert(" Bienvenido Usuario : " + user);
+
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  let usu = JSON.parse(localStorage.getItem('user'));
+  document.getElementById("welcome").innerHTML = usu.nombre;
 });
 
-function ingreso(user, pass){  
 
-    if (user.trim()==="" || pass.trim()===""){ //Chequea que el dato recibido no esté vacío. 
+function laproximasale() {
+  alert("Anota el pass en papel para la próxima");
 
-        alert("Ingrese datos correctos");
-    
-    }else{
-
-    localStorage.setItem("usuario", user.trim()); //setItem almacena el dato en la posición "usuario"
-    localStorage.setItem("password", pass.trim()); // Almaceno la contraseña
-    sessionStorage.setItem("usuario", user.trim());
-    location.href="inicio.html";
-    alert (" Bienvenido Usuario : " + user); 
-    
-    }
-}
-
-function laproximasale(){
-    alert("Anota el pass en papel para la próxima");
-    
 
 }
 
-function desconectate(){
-localStorage.clear();
-sessionStorage.clear();
-location.href="index.html";
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  var profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+
+  let google = {};
+  google.nombre = profile.getName();
+
+  localStorage.setItem("user", JSON.stringify(google));
+  location.href = "inicio.html";
+
+
 }
+function desconectate() {
+  localStorage.clear();
+  sessionStorage.clear();
+  location.href = "index.html";
+}
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function () {
+    console.log('User signed out.');
+    location.href="index.html";
+  });
+  var revokeAllScopes = function () {
+    gapi.auth2.getAuthInstance().disconnect();
+    
+  }
+}
+
--- js/products.js
+++ js/products.js
@@ -1,37 +1,149 @@
 var currentProductsArray = [];
+const ORDER_ASC_BY_PREC = "09";
+const ORDER_DESC_BY_PREC = "90";
+const ORDER_BY_PROD_SOLD = "Relev.";
+var currentSortCriteria = undefined;
+var minPrec = undefined;
+var maxPrec = undefined;
+
+
+function sortProducts(criteria, array) {
+  let result = [];
+  if (criteria === ORDER_ASC_BY_PREC) {
+    result = array.sort(function (a, b) {
+      if (a.cost < b.cost) { return -1; }
+      if (a.cost > b.cost) { return 1; }
+      return 0;
+    });
+  } else if (criteria === ORDER_DESC_BY_PREC) {
+    result = array.sort(function (a, b) {
+      if (a.cost > b.cost) { return -1; }
+      if (a.cost < b.cost) { return 1; }
+      return 0;
+    });
+  } else if (criteria === ORDER_BY_PROD_SOLD) {
+    result = array.sort(function (a, b) {
+      let aPrec = parseInt(a.soldCount);
+      let bPrec = parseInt(b.soldCount);
+
+      if (aPrec > bPrec) { return -1; }
+      if (aPrec < bPrec) { return 1; }
+      return 0;
+    });
+  }
+
+  return result;
+}
+
+
+
 
 function showProductsList() {
 
-    let htmlContentToAppend = "";
-    for (let i = 0; i < currentProductsArray.length; i++) {
-        let Prod = currentProductsArray[i];
+  let htmlContentToAppend = "";
+  for (let i = 0; i < currentProductsArray.length; i++) {
+    let Prod = currentProductsArray[i];
 
-            htmlContentToAppend += ` 
-   <div class="card-group";>
-        <div class="card" id="prod-list-container">
-          <img class="card-img-top" src="${Prod.imgSrc}" alt="Card image cap">
-          <div class="card-body">
-            <h5 class="card-title">${Prod.name}</h5>
-            <p class="card-text">${Prod.description}</p>
-            <h3 class="card-number">${Prod.cost} USD </h5>
-            <p class="card-text"><small class="text-muted">${Prod.soldCount} vendidos </small></p>
+    if (((minPrec == undefined) || (minPrec != undefined && parseInt(Prod.cost) >= minPrec)) &&
+      ((maxPrec == undefined) || (maxPrec != undefined && parseInt(Prod.cost) <= maxPrec))) {
+
+
+      htmlContentToAppend +=
+
+        ` <div class="card mb-4 box-shadow">
+                <img class="card-img-top" src="${Prod.imgSrc}">
+                <div class="card-body">
+                  <h5 class="card-number">${Prod.name}</h5>
+                  <p class="card-text">${Prod.description}</p>
+                  <div> <h5>${Prod.cost} USD  </h5>
+                  <p>${Prod.soldCount} Vendidos </p>
+                    </div>
+                  <div class="d-flex justify-content-between align-items-center">
+                    <div class="btn-group">
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Comprar</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary">Añadir al carrito</button>
+                    </div>
+                    
+                  </div>
+                  
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
-`
-
-            document.getElementById("prod-list-container").innerHTML = (htmlContentToAppend);
-        }
+      </div>
+      `
     }
+    document.getElementById("prod-list-container").innerHTML = htmlContentToAppend;
+  }
+}
 
+function sortAndShowProducts(sortCriteria, productsArray){
+  currentSortCriteria = sortCriteria;
+
+  if(productsArray != undefined){
+      currentProductsArray = productsArray;
+  }
+
+  currentProductsArray = sortProducts(currentSortCriteria, currentProductsArray);
+
+  //Muestro las categorías ordenadas
+  showProductsList();
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
-    getJSONData(PRODUCTS_URL).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            currentProductsArray = resultObj.data;
-            showProductsList(resultObj.data)
-        }
-    });
+  getJSONData(PRODUCTS_URL).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      currentProductsArray = resultObj.data;
+      sortAndShowProducts(ORDER_ASC_BY_PREC, resultObj.data);;
+    }
+  });
 });
+
+document.getElementById("sortAsc").addEventListener("click", function(){
+  sortAndShowProducts(ORDER_ASC_BY_PREC);
+});
+
+document.getElementById("sortDesc").addEventListener("click", function(){
+  sortAndShowProducts(ORDER_DESC_BY_PREC);
+});
+
+document.getElementById("sortBySold").addEventListener("click", function(){
+  sortAndShowProducts(ORDER_BY_PROD_SOLD);
+});
+
+document.getElementById("clearRangeFilterCost").addEventListener("click", function(){
+  document.getElementById("rangeFilterCostMin").value = "";
+  document.getElementById("rangeFilterCostMax").value = "";
+
+  minPrec = undefined;
+  maxPrec = undefined;
+
+  showProductsList();
+});
+
+document.getElementById("rangeFilterCost").addEventListener("click", function(){
+  //Obtengo el mínimo y máximo de los intervalos para filtrar por cantidad
+  //de productos por categoría.
+  minPrec = document.getElementById("rangeFilterCostMin").value;
+  maxPrec = document.getElementById("rangeFilterCostMax").value;
+
+  if ((minPrec != undefined) && (minPrec != "") && (parseInt(minPrec)) >= 0){
+      minPrec = parseInt(minPrec);
+  }
+  else{
+      minPrec = undefined;
+  }
+
+  if ((maxPrec != undefined) && (maxPrec != "") && (parseInt(maxPrec)) >= 0){
+      maxPrec = parseInt(maxPrec);
+  }
+  else{
+      maxPrec = undefined;
+  }
+
+  showProductsList();
+});
+
 
 
